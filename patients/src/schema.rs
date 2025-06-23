22933a487de78d3d79019665edd8a2c8959e515d
grewalrehmat <grewalrehmat8@gmail.com>
2025-06-23 11:42:50 +0530

    docker rust

--- patients/src/schema.rs
+++ patients/src/schema.rs
@@ -1,36 +1,58 @@
-// @generated automatically by Diesel CLI.
+diesel::table! {
+    employees (employeeid) {
+        employeeid -> Int4,
+        #[max_length = 100]
+        name -> Varchar,
+        #[max_length = 20]
+        phone_number -> Nullable<Varchar>,
+        #[max_length = 100]
+        email -> Varchar,
+        #[max_length = 255]
+        pwd -> Varchar,
+        #[max_length = 50]
+        role -> Varchar,
+    }
+}
 
 diesel::table! {
-    patients (id) {
-        id -> Int4,
+    patients (patientid) {
+        patientid -> Int4,
+        #[max_length = 100]
         name -> Varchar,
         age -> Int4,
+        #[max_length = 10]
         gender -> Varchar,
+        #[max_length = 20]
+        phone_number -> Nullable<Varchar>,
     }
 }
 
 diesel::table! {
-    reports (id) {
+    prescribe (id) {
+        employeeid -> Int4,
+        patientid -> Int4,
         id -> Int4,
-        patient_id -> Nullable<Int4>,
-        file_path -> Varchar,
-        uploaded_on -> Timestamp,
     }
 }
 
 diesel::table! {
-    users (id) {
-        id -> Int4,
-        email -> Varchar,
-        hashed_pw -> Varchar,
-        role -> Array<Nullable<Text>>,
+    reports (reportid) {
+        reportid -> Int4,
+        patientid -> Nullable<Int4>,
+        #[sql_name = "type"]
+        #[max_length = 100]
+        type_ -> Varchar,
+        date_uploaded -> Nullable<Timestamp>,
     }
 }
 
-diesel::joinable!(reports -> patients (patient_id));
+diesel::joinable!(prescribe -> employees (employeeid));
+diesel::joinable!(prescribe -> patients (patientid));
+diesel::joinable!(reports -> patients (patientid));
 
 diesel::allow_tables_to_appear_in_same_query!(
+    employees,
     patients,
+    prescribe,
     reports,
-    users,
 );
