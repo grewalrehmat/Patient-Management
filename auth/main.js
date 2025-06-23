cb0985eee132798419ecad6aadd28901a2dd02e2
baltej223 <baltej963@gmail.com>
2025-06-23 16:48:00 +0530

    Edit tsconfig.json

--- auth/main.js
+++ auth/main.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import express from 'express';
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
@@ -25,32 +16,32 @@ const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: JWT_SECRET
 };
-passport.use(new JwtStrategy(opts, (jwt_payload, done) => __awaiter(void 0, void 0, void 0, function* () {
+passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
     const user = users.find(u => u._id === jwt_payload.userId);
     if (user)
         return done(null, user);
     else
         return done(null, false);
-})));
+}));
 const app = express();
 app.use(express.json());
 app.use(passport.initialize());
-app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/login', async (req, res) => {
     const { email, password } = req.body;
     const user = users.find(u => u.email === email);
     if (!user)
         return res.status(401).json({ message: 'Invalid email or password' });
-    const isMatch = yield bcrypt.compare(password, user.passwordHash);
+    const isMatch = await bcrypt.compare(password, user.passwordHash);
     if (!isMatch)
         return res.status(401).json({ message: 'Invalid email or password' });
     const payload = { userId: user._id };
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
-}));
+});
 // POST /verify
 app.post('/verify', (req, res) => {
     const authHeader = req.headers.authorization;
-    const token = authHeader === null || authHeader === void 0 ? void 0 : authHeader.split(' ')[1];
+    const token = authHeader?.split(' ')[1];
     if (!token)
         return res.status(400).json({ error: 'Token missing' });
     try {
